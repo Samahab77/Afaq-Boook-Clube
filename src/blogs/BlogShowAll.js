5b8d537fa092e74c4da65dec12322eb8a15e3c17
samahab77 <s.bagido@gmail.com>
2019-07-29 17:52:09 +0300

    blog all

---
+++ src/blogs/BlogShowAll.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { showAll} from './api';
+import CommentIndex from '../comments/CommentIndex'
+import CommentCreate from '../comments/CommentCreate';
+import { showAllComment} from '../comments/api'
+// import { Link, Route } from 'react-router-dom'
+// import CommentIndex from '../comments/CommentIndex'
+// import CommentShow from '../comments/CommentShow'
+
+
+class BlogShowAll extends Component {
+    state = {
+        blog: {} //to return one object 
+    }
+
+    componentDidMount() {
+        const blogeId = this.props.blogId
+        console.log("bloge id", blogeId)
+
+        //for blogs
+        showAll(blogeId)
+            .then((response) => {
+                const showBlog = response.data.blog
+                console.log(response)
+                this.setState({
+                    blog: showBlog
+                })
+            })
+            .catch((error) => console.log(error));
+
+        //for comments
+        showAllComment(blogeId)
+        .then(res=>(
+            console.log(res)
+        ))
+        .catch(error => console.log(error))
+    }
+    render() {
+        // console.log("id", id)
+        const id = this.state.blogId
+
+        return (
+
+            <div>
+            <div>
+                <h1>{this.state.blog.title}</h1>
+                <img src={this.state.blog.img} alt="" />
+                <p>{this.state.blog.text}</p>
+                
+            </div>
+            <div>
+            {/* <CommentIndex user={this.props.user} key={id} blogId={id}/> */}
+            </div>
+                <div>
+                    
+                    <CommentIndex user={this.props.user} key={id} blogId={id} />
+                    {/* <CommentCreate updateComment={this.updateComment} user={this.props.user} blogId={this.props.blogId} /> */}
+
+                </div>
+                {/* <Link to='/blogs/blog:_id/comments'  >Create</Link>
+                <Route path='/blogs/blog:_id/comments'  component={CommentCreate} /> */}
+
+                
+                {/* <CommentShow user={this.props.user} blogId={id} key={id} /> */}
+
+            </div>
+
+        )
+    }
+}
+export default BlogShowAll ;
